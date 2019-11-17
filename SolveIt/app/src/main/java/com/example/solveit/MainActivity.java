9dfd2008e2f43db0011a585cb1ace316d0590a15
MrCrambo <kadirov.il.crambo@gmail.com>
2019-11-17 12:58:44 +0300

    displaying text from camera

--- SolveIt/app/src/main/java/com/example/solveit/MainActivity.java
+++ SolveIt/app/src/main/java/com/example/solveit/MainActivity.java
@@ -7,11 +7,12 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
@@ -26,50 +27,40 @@ public class MainActivity extends AppCompatActivity {
     TextView mTextView;
     CameraSource mCameraSource;
 
-    private static final String TAG = "MainActivity";
     private static final int requestPermissionID = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         mCameraView = findViewById(R.id.surfaceView);
         mTextView = findViewById(R.id.text_view);
 
         startCameraSource();
+
+        Toast.makeText(this, String.valueOf(MathEvaluation.evaluate("((4 - 2^3 + 1) * -sqrt(3*3+4*4)) / 2")), Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode != requestPermissionID) {
-            Log.d(TAG, "Got unexpected permission result: " + requestCode);
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-            return;
-        }
-
-        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        if ( requestCode == requestPermissionID && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             try {
-                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                    return;
-                }
                 mCameraSource.start(mCameraView.getHolder());
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        } else {
+            Toast.makeText(this, "Permission not granted", Toast.LENGTH_LONG).show();
         }
     }
 
     private void startCameraSource() {
 
-        //Create the TextRecognizer
         final TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
 
-        if (!textRecognizer.isOperational()) {
-            Log.w(TAG, "Detector dependencies not loaded yet");
-        } else {
-
-            //Initialize camera source to use high resolution and set Auto focus on.
+        if (textRecognizer.isOperational()) {
             mCameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer)
                     .setFacing(CameraSource.CAMERA_FACING_BACK)
                     .setRequestedPreviewSize(1280, 1024)
@@ -77,10 +68,6 @@ public class MainActivity extends AppCompatActivity {
                     .setRequestedFps(2.0f)
                     .build();
 
-            /*
-             * Add call back to SurfaceView and check if camera permission is granted.
-             * If permission is granted we can start our cameraSource and pass it to surfaceView
-             */
             mCameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
                 @Override
                 public void surfaceCreated(SurfaceHolder holder) {
@@ -92,7 +79,6 @@ public class MainActivity extends AppCompatActivity {
                             ActivityCompat.requestPermissions(MainActivity.this,
                                     new String[]{Manifest.permission.CAMERA},
                                     requestPermissionID);
-                            return;
                         }
                         mCameraSource.start(mCameraView.getHolder());
                     } catch (IOException e) {
@@ -110,32 +96,23 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
-            //Set the TextRecognizer's Processor.
             textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
                 @Override
                 public void release() {
                 }
 
-                /**
-                 * Detect all the text from camera using TextBlock and the values into a stringBuilder
-                 * which will then be set to the textView.
-                 * */
                 @Override
                 public void receiveDetections(Detector.Detections<TextBlock> detections) {
                     final SparseArray<TextBlock> items = detections.getDetectedItems();
                     if (items.size() != 0 ){
 
-                        mTextView.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                StringBuilder stringBuilder = new StringBuilder();
-                                for(int i = 0; i < items.size(); i++){
-                                    TextBlock item = items.valueAt(i);
-                                    stringBuilder.append(item.getValue());
-                                    stringBuilder.append("\n");
-                                }
-                                mTextView.setText(stringBuilder.toString());
+                        mTextView.post(() -> {
+                            StringBuilder stringBuilder = new StringBuilder();
+                            for(int i = 0; i < items.size(); i++){
+                                TextBlock item = items.valueAt(i);
+                                stringBuilder.append(item.getValue()).append("\n");
                             }
+                            mTextView.setText(stringBuilder.toString());
                         });
                     }
                 }
